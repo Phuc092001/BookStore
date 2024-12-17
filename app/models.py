dc41d153b6c16cd13ae395e2295aa70eabf9357c
Phuc092001 <phuc670481@gmail.com>
2024-12-17 10:09:45 +0700

    Update requirements

--- app/models.py
+++ app/models.py
@@ -115,70 +115,70 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        # c1 = Category(name='Sách pháp luật')
-        # c2 = Category(name='Sách thiếu nhi')
-        # c3 = Category(name='Sách khoa học')
-        # c4 = Category(name='Sách ngoại ngữ')
-        # db.session.add_all([c1, c2, c3, c4])
-        # db.session.commit()
+        c1 = Category(name='Sách pháp luật')
+        c2 = Category(name='Sách thiếu nhi')
+        c3 = Category(name='Sách khoa học')
+        c4 = Category(name='Sách ngoại ngữ')
+        db.session.add_all([c1, c2, c3, c4])
+        db.session.commit()
 
-        # products = [{
-        #     "id": 1,
-        #     "name": "Bài Tập Giáo Dục Kinh Tế Và Pháp Luật 12",
-        #     "price": 17500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731030498/g6wsgnaka7ycusdcef2a.png",
-        #     "quantity": 10,
-        #     "category_id": 1
-        # }, {
-        #     "id": 2,
-        #     "name": "Quy Định Pháp Luật Về Cơ Chế, Chính Sách Hỗ Trợ Sản Xuất Nông Nghiệp Để Khôi Phục Sản Xuất Vùng Bị Thiệt Hại Do Thiên Tai, Dịch Bệnh",
-        #     "price": 20500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731046959/qycfemeqcactndq2hlqw.png",
-        #     "quantity": 15,
-        #     "category_id": 1
-        # }, {
-        #     "id": 3,
-        #     "name": "Chính Sách Pháp Luật Tố Tụng Dân Sự Đáp Ứng Yêu Cầu Của Cuộc Cách Mạng Công Nghiệp Lần Thứ Tư Ở Việt Nam",
-        #     "price": 21500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047084/icbntpxoojpbtcqvblud.png",
-        #     "quantity": 20,
-        #     "category_id": 1
-        # }, {
-        #     "id": 4,
-        #     "name": "Doraemon - Tranh Truyện Màu - Năm 2112 Doraemon",
-        #     "price": 21500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047233/kdxtuohscmpe5k4bu6n1.png",
-        #     "quantity": 20,
-        #     "category_id": 2
-        # }, {
-        #     "id": 5,
-        #     "name": "Doraemon - Tranh Truyện Màu - Năm 2112 Doraemon",
-        #     "price": 21500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047367/ehmmkyj6ht1gheiomtmi.png",
-        #     "quantity": 20,
-        #     "category_id": 2
-        # }, {
-        #     "id": 6,
-        #     "name": "Cuốn Sách Khoa Học Đầu Tiên Dành Cho Học Sinh Tiểu Học - Câu Chuyện Về Biến Đổi Khí Hậu",
-        #     "price": 21500,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047458/jtb3n5tsffhkpb6fm7us.png",
-        #     "quantity": 20,
-        #     "category_id": 3
-        # }, {
-        #     "id": 7,
-        #     "name": "Giáo Trình Chuẩn HSK 5 - Sách Bài Tập - Tập 1",
-        #     "price": 150000,
-        #     "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047559/xcbi4dpirjuln1pxbheq.png",
-        #     "quantity": 20,
-        #     "category_id": 4
-        # }]
-        #
-        # for p in products:
-        #     prod = Product(**p)
-        #     db.session.add(prod)
-        #
-        # db.session.commit()
+        products = [{
+            "id": 1,
+            "name": "Bài Tập Giáo Dục Kinh Tế Và Pháp Luật 12",
+            "price": 17500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731030498/g6wsgnaka7ycusdcef2a.png",
+            "quantity": 10,
+            "category_id": 1
+        }, {
+            "id": 2,
+            "name": "Quy Định Pháp Luật Về Cơ Chế, Chính Sách Hỗ Trợ Sản Xuất Nông Nghiệp Để Khôi Phục Sản Xuất Vùng Bị Thiệt Hại Do Thiên Tai, Dịch Bệnh",
+            "price": 20500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731046959/qycfemeqcactndq2hlqw.png",
+            "quantity": 15,
+            "category_id": 1
+        }, {
+            "id": 3,
+            "name": "Chính Sách Pháp Luật Tố Tụng Dân Sự Đáp Ứng Yêu Cầu Của Cuộc Cách Mạng Công Nghiệp Lần Thứ Tư Ở Việt Nam",
+            "price": 21500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047084/icbntpxoojpbtcqvblud.png",
+            "quantity": 20,
+            "category_id": 1
+        }, {
+            "id": 4,
+            "name": "Doraemon - Tranh Truyện Màu - Năm 2112 Doraemon",
+            "price": 21500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047233/kdxtuohscmpe5k4bu6n1.png",
+            "quantity": 20,
+            "category_id": 2
+        }, {
+            "id": 5,
+            "name": "Doraemon - Tranh Truyện Màu - Năm 2112 Doraemon",
+            "price": 21500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047367/ehmmkyj6ht1gheiomtmi.png",
+            "quantity": 20,
+            "category_id": 2
+        }, {
+            "id": 6,
+            "name": "Cuốn Sách Khoa Học Đầu Tiên Dành Cho Học Sinh Tiểu Học - Câu Chuyện Về Biến Đổi Khí Hậu",
+            "price": 21500,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047458/jtb3n5tsffhkpb6fm7us.png",
+            "quantity": 20,
+            "category_id": 3
+        }, {
+            "id": 7,
+            "name": "Giáo Trình Chuẩn HSK 5 - Sách Bài Tập - Tập 1",
+            "price": 150000,
+            "image": "https://res.cloudinary.com/dsvtzkvti/image/upload/v1731047559/xcbi4dpirjuln1pxbheq.png",
+            "quantity": 20,
+            "category_id": 4
+        }]
 
-        # u = User(name='Admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), user_role=UserRoleEnum.ADMIN)
-        # db.session.add(u)
-        # db.session.commit()
+        for p in products:
+            prod = Product(**p)
+            db.session.add(prod)
+
+        db.session.commit()
+
+        u = User(name='Admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), user_role=UserRoleEnum.ADMIN)
+        db.session.add(u)
+        db.session.commit()
